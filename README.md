2850b64aa70436cefb384673058f88cbdbf018a4
wenhx <3909713+wenhx@users.noreply.github.com>
2020-09-03 21:11:13 +0800

    DirectoryEverything: an Everything helper tool.

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 ## FtpUploader
 非交互式命令行Ftp上传工具。
+
+##DirectoryEverything  
+An [Everything](https://www.voidtools.com/) helper tool uses the SDK of Everything to search all files under the special folder.
+一个[Everything](https://www.voidtools.com/) 软件的帮助小工具，它通过Everything的接口来批量查找指定目录下面的所有文件。
---
+++ src/DirectoryEverything/DirectoryEverything/EverythingSearchedResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectoryEverything
+{
+    class EverythingSearchedResult
+    {
+        public string Keyword { get; set; }
+        public string OriginalFileSize { get; set; }
+        public int Total { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public long Size { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public string TotalString
+        {
+            get
+            {
+                if (Total > 0)
+                    return Total.ToString();
+                return string.Empty;
+            }
+        }
+    }
+}
